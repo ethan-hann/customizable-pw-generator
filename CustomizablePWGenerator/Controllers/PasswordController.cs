c3ad1d3d53b618ca52964b397434d3a90c3988a8
ethan-hann <36464732+ethan-hann@users.noreply.github.com>
2022-10-03 11:30:09 -0500

    Initial Commit

---
+++ CustomizablePWGenerator/Controllers/PasswordController.cs
@@ -0,0 +1,21 @@
+﻿using CustomizablePWGenerator.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CustomizablePWGenerator.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PasswordController : ControllerBase
+    {
+        Password generator = new Password();
+
+        [Route("getPassword")]
+        [HttpGet]
+        public Password GetPassword(string templateText)
+        {
+            generator.TemplateText = templateText;
+            return generator.GeneratePassword();
+        }
+    }
+}
---
+++ CustomizablePWGenerator/Models/Password.cs
@@ -0,0 +1,26 @@
+﻿namespace CustomizablePWGenerator.Models
+{
+    public class Password
+    {
+        /// <summary>
+        /// The template text that defines this password.
+        /// </summary>
+        public string TemplateText { get; set; } = string.Empty;
+
+        ///// <summary>
+        ///// The regular expression that defines this password output.
+        ///// </summary>
+        //public string Regex { get; set; } = string.Empty;
+
+        public string GeneratedPassword { get; set; } = string.Empty;
+
+        /// <summary>
+        /// Generates a password based on <see cref="TemplateText"/>.
+        /// </summary>
+        /// <returns>An instance of the generated password.</returns>
+        public Password GeneratePassword()
+        {
+
+        }
+    }
+}
